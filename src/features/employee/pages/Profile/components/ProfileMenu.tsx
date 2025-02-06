6b0f40bb4e2b2995d01038ae335f4c658148efea
dianlucky13 <dianlucky13@gmail.com>
2025-02-06 23:11:45 +0800

    [Profile] Add : Menambahkan halaman profile

---
+++ src/features/employee/pages/Profile/components/ProfileMenu.tsx
@@ -0,0 +1,35 @@
+import { Divider } from "@mantine/core";
+import { IconChevronRight, IconLogout, IconUser } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
+
+export const ProfileMenu: React.FC = () => {
+    const navigate = useNavigate();
+  return (
+    <section className="w-full mt-8 px-7 flex flex-col divide-y divide-gray-300">
+      {/* Data Diri */}
+      <button
+        onClick={() => navigate("/profile/biodata")}
+        className="bg-transparent text-left flex w-full items-center py-3 hover:bg-slate-100 ps-2"
+      >
+        <div className="bg-brown text-white rounded-lg p-2">
+          <IconUser size={25} />
+        </div>
+        <div className="font-semibold px-4 flex-grow text-sm">Data Diri</div>
+        <IconChevronRight size={25} />
+      </button>
+
+      <Divider my={'md'}></Divider>
+
+      <button
+        // onClick={() => logout()}
+        className="bg-transparent text-left flex w-full items-center py-3 hover:bg-slate-100 ps-2"
+      >
+        <div className="bg-red-600 text-white rounded-lg p-2">
+          <IconLogout size={25} />
+        </div>
+        <div className="font-semibold px-4 flex-grow text-sm">Logout</div>
+        <IconChevronRight size={25} />
+      </button>
+    </section>
+  );
+};
--- src/features/employee/pages/Profile/pages/ProfilePage.tsx
+++ src/features/employee/pages/Profile/pages/ProfilePage.tsx
@@ -1,29 +1,25 @@
-import { IconChevronLeft } from "@tabler/icons-react";
+import { IconBriefcase, IconChevronLeft, IconUser } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
+import { ProfileMenu } from "../components";
 
 export const ProfilePage: React.FC = () => {
   const navigate = useNavigate();
   return (
     <main>
-      <section className="w-full h-20 bg-brown rounded-b-3xl"></section>
-
-      <section className="bg-white mx-5 p-3 shadow-md rounded-lg flex flex-col gap-2 -mt-10">
-        <div className="grid grid-cols-12">
-          <div className="col-span-1">
-            <IconChevronLeft
-              onClick={() => {
-                navigate(-1);
-              }}
-              size={21}
-              className="font-bold rounded-md"
-            />
+      <section className="flex m-3 shadow-md rounded-xl gap-4 items-center p-4 bg-white mt-15">
+        <div className="bg-slate-500 text-white rounded-full p-4">
+          <IconUser className="w-10 h-10" />
+        </div>
+        <div className="font-bold text-lg">
+          Pegawai 1
+          <div className="text-sm text-gray-600 flex gap-1 items-center">
+            <IconBriefcase size={20} />{" "}
+            <span className="font-semibold">Pegawai tetap</span>
           </div>
-          <div className="col-span-11 text-center -ml-4 mr-3 font-semibold text-brown">
-            <h2 className="font-semibold">Profil</h2>
-          </div>
-          {/* </div> */}
         </div>
       </section>
+
+      <ProfileMenu />
     </main>
   );
 };
