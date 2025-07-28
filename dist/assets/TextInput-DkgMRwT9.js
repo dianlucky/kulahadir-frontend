230a6fb88dc6b45969dabfb8dc9552eda8447de8
dianlucky13 <dianlucky13@gmail.com>
2025-07-28 21:17:10 +0800

    build for /kulakita deployment

---
+++ dist/assets/TextInput-DkgMRwT9.js
@@ -0,0 +1,1 @@
+import{f as n,u as a,j as r}from"./index-D13YgGcN.js";import{I as t}from"./InputBase-BJOZdEhw.js";const u={},s=n((e,o)=>{const p=a("TextInput",u,e);return r.jsx(t,{component:"input",ref:o,...p,__staticSelector:"TextInput"})});s.classes=t.classes;s.displayName="@mantine/core/TextInput";export{s as T};
